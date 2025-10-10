e895674da41b2f14afd856cb85d06457a73d6283
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2025-10-09 20:28:24 -0400

    Fixed folder user in file path for client.html and styles.css  ./client -> ./hosted

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
-const index = fs.readFileSync(`${__dirname}/../client/client.html`);
-const css = fs.readFileSync(`${__dirname}/../client/styles.css`);
+const index = fs.readFileSync(`${__dirname}/../hosted/client.html`);
+const css = fs.readFileSync(`${__dirname}/../hosted/styles.css`);
 
 const getIndex = (request, response) => {
     response.writeHead(200, {'Content-Type': 'text/html'});

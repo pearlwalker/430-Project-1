d07b6264e6a56074c025877e2d705f9a4b288cb7
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2025-10-09 20:17:34 -0400

    urlStruct method that routes request URLs to appropriate endpoints.

--- src/server.js
+++ src/server.js
@@ -2,6 +2,12 @@ const http = require('http');
 const htmlHandler = require('./htmlResponses.js');
 const jsonHandler = require('./jsonResponses.js');
 
+const urlStruct = {
+    '/': htmlHandler.getIndex,
+    '/styles.css': htmlHandler.getCSS,
+    notFound: jsonHandler.error404
+};
+
 const onRequest = (request, response) => {
   const protocol = request.connection.encrypted ? 'https' : 'http';
   const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);

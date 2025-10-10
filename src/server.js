9d8a70ab1c56e43f89078321a78a8eaef19e61f4
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2025-10-09 20:24:19 -0400

    Specified which port this server should run on.

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const http = require('http');
 const htmlHandler = require('./htmlResponses.js');
 const jsonHandler = require('./jsonResponses.js');
 
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
 const urlStruct = {
     '/': htmlHandler.getIndex,
     '/styles.css': htmlHandler.getCSS,

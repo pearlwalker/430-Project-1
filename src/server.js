4d79e067f9136c0031916aab5c9d098fca88bb2a
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2025-10-09 20:12:20 -0400

    Code to start server using (unfinished) onRequest method. Also, deleted a comment.

--- src/server.js
+++ src/server.js
@@ -3,7 +3,10 @@ const htmlHandler = require('./htmlResponses.js');
 const jsonHandler = require('./jsonResponses.js');
 
 const onRequest = (request, response) => {
-    // if encrypted then https else http
   const protocol = request.connection.encrypted ? 'https' : 'http';
   const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
 };
+
+http.createServer(onRequest).listen(prototype, () => {
+    console.log(`Listening on 127.0.0.1: ${port}`);
+});

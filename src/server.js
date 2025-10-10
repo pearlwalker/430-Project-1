06298add90a539164c6166fa2c3cc51976c4fb08
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2025-10-09 20:09:36 -0400

    Created onRequest method to handle server requests. Contains 2 local constants: protocol and parsedUrl.

--- src/server.js
+++ src/server.js
@@ -1,3 +1,9 @@
 const http = require('http');
 const htmlHandler = require('./htmlResponses.js');
 const jsonHandler = require('./jsonResponses.js');
+
+const onRequest = (request, response) => {
+    // if encrypted then https else http
+  const protocol = request.connection.encrypted ? 'https' : 'http';
+  const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
+};

4eb5188c96cf532a416309ae5a1b4b53ac849f2c
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2025-10-14 19:19:09 -0400

    Format document

--- hosted/bundle.js
+++ hosted/bundle.js
@@ -15,18 +15,21 @@
   \**************************/
 /***/ (() => {
 
-eval("{\n\n//# sourceURL=webpack://430-project-1/./client/client.js?\n}");
+        eval("{\n\n//# sourceURL=webpack://430-project-1/./client/client.js?\n}");
 
-/***/ })
+        /***/
+})
 
-/******/ 	});
+    /******/
+});
 /************************************************************************/
-/******/ 	
+/******/
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
 /******/ 	var __webpack_exports__ = {};
 /******/ 	__webpack_modules__["./client/client.js"]();
-/******/ 	
-/******/ })()
-;
+  /******/
+  /******/
+})()
+  ;

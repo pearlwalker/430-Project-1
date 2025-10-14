b6743f948d6f09ed601cb53f2b1a6ecf1b80ce3f
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2025-10-14 19:39:53 -0400

    For/of loop with empty event listener

--- client/client.js
+++ client/client.js
@@ -2,7 +2,11 @@ const collapsible = document.getElementsByClassName('collapsible');
 const requestForm = document.getElementsByClassName('requestForm');
 
 const collapsibleEvents = () => {
-
+    for (const element of collapsible) {
+        element.addEventListener("click", () => {
+            
+        })
+    };
 };
 
 const handleResponse = async (response) => {

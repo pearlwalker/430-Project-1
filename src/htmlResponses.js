d1d479e8fcf53d9ba79e646c12aecd89b78862cb
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2025-10-14 18:12:57 -0400

    Loaded client.js into memory Writing code to serve the static client.js file

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const index = fs.readFileSync(`${__dirname}/../hosted/client.html`);
 const css = fs.readFileSync(`${__dirname}/../hosted/styles.css`);
 const bundle = fs.readFileSync(`${__dirname}/../hosted/bundle.js`);
+const client = fs.readFileSync(`${__dirname}/../client/client.js`);
 const countries = fs.readFileSync(`${__dirname}/../data/countries.json`);
 
 const serveFile = (response, file, contentType) => {
